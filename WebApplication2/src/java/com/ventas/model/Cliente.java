757fbd195f414d074fc86514263922d5e9fd4a8a
Victor Jimenez <vjimenez@americatv.com.pe>
2016-07-13 12:47:57 -0500

    avance

---
+++ WebApplication2/src/java/com/ventas/model/Cliente.java
@@ -0,0 +1,177 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ventas.model;
+
+/**
+ *
+ * @author vjimenez
+ */
+public class Cliente {
+   private int cCod;
+  private String cRuc;
+  private String cRzS;
+  private String cDir;
+  private String cLug;
+  private String cMap;
+  private String cFnc;
+  private String cTl1;
+  private String cTl2;
+  private String cCl1;
+  private String cCl2;
+  private String cCe1;
+  private String cCe2;
+  private String cFio;
+  private String cCat;
+  private String cPds;
+  private Vendedor vCod;
+  private String cFot;
+
+    public int getcCod() {
+        return cCod;
+    }
+
+    public void setcCod(int cCod) {
+        this.cCod = cCod;
+    }
+
+    public String getcRuc() {
+        return cRuc;
+    }
+
+    public void setcRuc(String cRuc) {
+        this.cRuc = cRuc;
+    }
+
+    public String getcRzS() {
+        return cRzS;
+    }
+
+    public void setcRzS(String cRzS) {
+        this.cRzS = cRzS;
+    }
+
+    public String getcDir() {
+        return cDir;
+    }
+
+    public void setcDir(String cDir) {
+        this.cDir = cDir;
+    }
+
+    public String getcLug() {
+        return cLug;
+    }
+
+    public void setcLug(String cLug) {
+        this.cLug = cLug;
+    }
+
+    public String getcMap() {
+        return cMap;
+    }
+
+    public void setcMap(String cMap) {
+        this.cMap = cMap;
+    }
+
+    public String getcFnc() {
+        return cFnc;
+    }
+
+    public void setcFnc(String cFnc) {
+        this.cFnc = cFnc;
+    }
+
+    public String getcTl1() {
+        return cTl1;
+    }
+
+    public void setcTl1(String cTl1) {
+        this.cTl1 = cTl1;
+    }
+
+    public String getcTl2() {
+        return cTl2;
+    }
+
+    public void setcTl2(String cTl2) {
+        this.cTl2 = cTl2;
+    }
+
+    public String getcCl1() {
+        return cCl1;
+    }
+
+    public void setcCl1(String cCl1) {
+        this.cCl1 = cCl1;
+    }
+
+    public String getcCl2() {
+        return cCl2;
+    }
+
+    public void setcCl2(String cCl2) {
+        this.cCl2 = cCl2;
+    }
+
+    public String getcCe1() {
+        return cCe1;
+    }
+
+    public void setcCe1(String cCe1) {
+        this.cCe1 = cCe1;
+    }
+
+    public String getcCe2() {
+        return cCe2;
+    }
+
+    public void setcCe2(String cCe2) {
+        this.cCe2 = cCe2;
+    }
+
+    public String getcFio() {
+        return cFio;
+    }
+
+    public void setcFio(String cFio) {
+        this.cFio = cFio;
+    }
+
+    public String getcCat() {
+        return cCat;
+    }
+
+    public void setcCat(String cCat) {
+        this.cCat = cCat;
+    }
+
+    public String getcPds() {
+        return cPds;
+    }
+
+    public void setcPds(String cPds) {
+        this.cPds = cPds;
+    }
+
+    public Vendedor getvCod() {
+        return vCod;
+    }
+
+    public void setvCod(Vendedor vCod) {
+        this.vCod = vCod;
+    }
+
+    public String getcFot() {
+        return cFot;
+    }
+
+    public void setcFot(String cFot) {
+        this.cFot = cFot;
+    }
+  
+  
+}
