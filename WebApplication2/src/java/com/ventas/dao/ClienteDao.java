7b17caa048ae387884e81cdd9e0881cb42d8ef52
Victor Jimenez <vjimenez@americatv.com.pe>
2016-07-14 09:25:11 -0500

    ssss

--- WebApplication2/src/java/com/ventas/dao/ClienteDao.java
+++ WebApplication2/src/java/com/ventas/dao/ClienteDao.java
@@ -106,27 +106,24 @@ public List<Cliente> listar() throws Exception{
            rs =st.executeQuery();
            while (rs.next()) {
            clis = new Cliente();
-          clis.setvCod(rs.getString("vCod"));
-          clis.setVruc(rs.getString("vRuc"));
-          clis.setVrzs(rs.getString("vRzS"));
-          clis.setVdir(rs.getString("vDir"));
-          clis.setVlug(rs.getString("vLug"));
-          clis.setVmap(rs.getString("vMap"));
-          clis.setVfnc(rs.getString("vFnc"));
-          clis.setVtlf(rs.getString("vTlf"));
-          clis.setVcl1(rs.getString("vCl1"));
-          clis.setVcl2(rs.getString("vCl2"));
-          clis.setVce1(rs.getString("vCe1"));
-          clis.setVce2(rs.getString("vCe2"));
-          clis.setVcom(rs.getFloat("vCom"));
-          clis.setVfio(rs.getString("vFio"));
-          clis.setVffo(rs.getString("vFfo"));
-          clis.setVmcs(rs.getString("vMcs"));
-          bytes= rs.getBytes("vFot");
-          clis.setVrft(rs.getString("vRft"));
-          clis.setVusr(rs.getString("vUsr"));
-          clis.setVpas(rs.getString("vPas"));
-          clis.setVacc(rs.getString("vAcc"));
+          clis.setCcod(rs.getInt("cCod"));
+          clis.setCruc(rs.getString("cRuc"));
+          clis.setCrzs(rs.getString("cRzS"));
+          clis.setCdir(rs.getString("cDir"));
+          clis.setClug(rs.getString("cLug"));
+          clis.setCmap(rs.getString("cMap"));
+          clis.setCfnc(rs.getString("cFnc"));
+          clis.setCtl1(rs.getString("cTl1"));
+          clis.setCtl2(rs.getString("cTl2"));
+          clis.setCcl1(rs.getString("cCl1"));
+          clis.setCcl2(rs.getString("cCl2"));
+          clis.setCce1(rs.getString("cCe1"));
+          clis.setCce2(rs.getString("cCe2"));
+          clis.setCfio(rs.getString("cfio"));
+          clis.setCcat(rs.getString("cCat"));
+          clis.setCpds(rs.getString("cPds"));
+          //cli.setC(rs.getString("vMcs"));
+          clis.setCfot(rs.getBytes("cFot"));//wwwww
             
 
               }
@@ -140,7 +137,7 @@ public List<Cliente> listar() throws Exception{
        return clis;
 }
           
-public void modificar(Cliente ven) throws Exception{
+public void modificar(Cliente cli) throws Exception{
        try {
 //         DateFormat ft = new SimpleDateFormat("yyyy-MM-dd");
 //         java.util.Date fecha = null; // crea objetos tipo util.Date y sql.Date
@@ -149,27 +146,21 @@ public void modificar(Cliente ven) throws Exception{
 //         fecha2 = new java.sql.Date(fecha.getTime());
        this.Conectar();
            PreparedStatement st = this.getCn().prepareStatement("UPDATE cliente SET vRuc=?,vRzS=?,vDir=?,vLug=?,vMap=?,vFnc=STR_TO_DATE(?,'%d/%m/%Y'),vTlf=?,vCl1=?,vCl2=?,vCe1=?,vCe2=?,vCom=?,vRft=?,vFot=?,vFio=STR_TO_DATE(?,'%d/%m/%Y'),vFfo=STR_TO_DATE(?,'%d/%m/%Y'),vMcs=?,vUsr=?,vPas=?,vAcc=? WHERE vCod = ?");
-           st.setString(1,ven.getCruc());
-           st.setString(2, ven.getVrzs());
-           st.setString(3, ven.getVdir());
-           st.setString(4, ven.getVlug());
-           st.setString(5, ven.getVmap());
-           st.setString(6, ven.getVfnc());
-           st.setString(7, ven.getVtlf());
-           st.setString(8, ven.getVcl1());
-           st.setString(9, ven.getVcl2());
-           st.setString(10, ven.getVce1());
-           st.setString(11, ven.getVce2());
-           st.setFloat(12, ven.getVcom());
-           st.setString(13,ven.getVrft());
-           st.setBytes(14,ven.getVfot());
-           st.setString(15, ven.getVfio());
-           st.setString(16, ven.getVffo());
-           st.setString(17, ven.getVmcs());
-           st.setString(18, ven.getVusr());
-           st.setString(19, ven.getVpas());
-           st.setString(20, ven.getVacc());
-           st.setString(21,ven.getvCod());
+            st.setString(1,cli.getCruc());
+           st.setString(2, cli.getCrzs());
+           st.setString(3, cli.getCdir());
+           st.setString(4, cli.getClug());
+           st.setString(5, cli.getCmap());
+           st.setString(6, cli.getCfnc());
+           st.setString(7, cli.getCtl1());
+           st.setString(8, cli.getCtl2());
+           st.setString(9, cli.getCce1());
+           st.setString(10, cli.getCce2());
+           st.setString(11, cli.getCfio());
+           st.setString(12, cli.getCcat());
+           st.setString(13, cli.getCpds());
+           //st.setString(14, cli.getC);
+           st.setBytes(15, cli.getCfot());
            st.executeUpdate();
        } catch (Exception e) {
        FacesContext context = FacesContext.getCurrentInstance();
@@ -181,11 +172,11 @@ public void modificar(Cliente ven) throws Exception{
        }
    
    } 
-    public void eliminar(Cliente ven) throws Exception{
+    public void eliminar(Cliente cli) throws Exception{
        try {
        this.Conectar();
-           PreparedStatement st = this.getCn().prepareStatement("DELETE FROM cliente  WHERE vCod = ?");
-           st.setString(1,ven.getvCod());
+           PreparedStatement st = this.getCn().prepareStatement("DELETE FROM cliente  WHERE cCod = ?");
+           st.setInt(1,cli.getCcod());
            st.executeUpdate();
        } catch (Exception e) {
        throw e;
@@ -196,21 +187,6 @@ public void modificar(Cliente ven) throws Exception{
    
    }
     
-      private String getparsedDate(String date) throws Exception {
-        DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS", Locale.US);
-        String s1 = date;
-        String s2 = null;
-        Date d;
-        try {
-            d = sdf.parse(s1);
-            s2 = (new SimpleDateFormat("yyyy-MM-dd")).format(d);
-
-        } catch (ParseException e) {
-        }
-
-        return s2;
-
-    }
             
             
 }
