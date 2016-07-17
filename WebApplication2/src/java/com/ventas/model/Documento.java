c311290b43e7432a27f53da69b743f8d5e736e51
Victor Jimenez <vtorero@gmail.com>
2016-07-17 00:27:43 -0500

    ventas

---
+++ WebApplication2/src/java/com/ventas/model/Documento.java
@@ -0,0 +1,168 @@
+
+package com.ventas.model;
+
+public class Documento {
+private int cdoc ;
+private int demp;
+private String dnro;
+private String dfch;
+private int dcli;
+private String dmon;  
+private double dtic;
+private double dbrt;  
+private double dcds;
+private double ddsc;  
+private double digv;  
+private double dtig;
+private double dtnt;  
+private double dtpg;
+private int ddif; 
+private double dcom;
+private double dcit;
+private String dest;
+
+    public int getCdoc() {
+        return cdoc;
+    }
+
+    public void setCdoc(int cdoc) {
+        this.cdoc = cdoc;
+    }
+
+    public int getDemp() {
+        return demp;
+    }
+
+    public void setDemp(int demp) {
+        this.demp = demp;
+    }
+
+    public String getDnro() {
+        return dnro;
+    }
+
+    public void setDnro(String dnro) {
+        this.dnro = dnro;
+    }
+
+    public String getDfch() {
+        return dfch;
+    }
+
+    public void setDfch(String dfch) {
+        this.dfch = dfch;
+    }
+
+    public int getDcli() {
+        return dcli;
+    }
+
+    public void setDcli(int dcli) {
+        this.dcli = dcli;
+    }
+
+    public String getDmon() {
+        return dmon;
+    }
+
+    public void setDmon(String dmon) {
+        this.dmon = dmon;
+    }
+
+    public double getDtic() {
+        return dtic;
+    }
+
+    public void setDtic(double dtic) {
+        this.dtic = dtic;
+    }
+
+    public double getDbrt() {
+        return dbrt;
+    }
+
+    public void setDbrt(double dbrt) {
+        this.dbrt = dbrt;
+    }
+
+    public double getDcds() {
+        return dcds;
+    }
+
+    public void setDcds(double dcds) {
+        this.dcds = dcds;
+    }
+
+    public double getDdsc() {
+        return ddsc;
+    }
+
+    public void setDdsc(double ddsc) {
+        this.ddsc = ddsc;
+    }
+
+    public double getDigv() {
+        return digv;
+    }
+
+    public void setDigv(double digv) {
+        this.digv = digv;
+    }
+
+    public double getDtig() {
+        return dtig;
+    }
+
+    public void setDtig(double dtig) {
+        this.dtig = dtig;
+    }
+
+    public double getDtnt() {
+        return dtnt;
+    }
+
+    public void setDtnt(double dtnt) {
+        this.dtnt = dtnt;
+    }
+
+    public double getDtpg() {
+        return dtpg;
+    }
+
+    public void setDtpg(double dtpg) {
+        this.dtpg = dtpg;
+    }
+
+    public int getDdif() {
+        return ddif;
+    }
+
+    public void setDdif(int ddif) {
+        this.ddif = ddif;
+    }
+
+    public double getDcom() {
+        return dcom;
+    }
+
+    public void setDcom(double dcom) {
+        this.dcom = dcom;
+    }
+
+    public double getDcit() {
+        return dcit;
+    }
+
+    public void setDcit(double dcit) {
+        this.dcit = dcit;
+    }
+
+    public String getDest() {
+        return dest;
+    }
+
+    public void setDest(String dest) {
+        this.dest = dest;
+    }
+
+}
