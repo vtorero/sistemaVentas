efdeb505233a798199cb28088d9efa2c9b0b3792
Victor Jimenez <vjimenez@americatv.com.pe>
2016-07-19 14:24:22 -0500

    vebtas

---
+++ WebApplication2/src/java/com/ventas/model/ItemDocumento.java
@@ -0,0 +1,160 @@
+
+package com.ventas.model;
+
+public class ItemDocumento {
+    private int icod;
+    private int iemp;
+    private String itip;
+    private String inro;
+    private int inum;
+    private int iart;
+    private String iDs1;
+    private String iuvt;
+    private double ipru;
+    private double icom;
+    private int icnt;
+    private double ibrt;
+    private double idsc;
+    private double itai;
+    private double iigv;
+    private double itnt;
+    private String iest;
+
+    public int getIcod() {
+        return icod;
+    }
+
+    public void setIcod(int icod) {
+        this.icod = icod;
+    }
+
+    public int getIemp() {
+        return iemp;
+    }
+
+    public void setIemp(int iemp) {
+        this.iemp = iemp;
+    }
+
+    public String getItip() {
+        return itip;
+    }
+
+    public void setItip(String itip) {
+        this.itip = itip;
+    }
+
+    public String getInro() {
+        return inro;
+    }
+
+    public void setInro(String inro) {
+        this.inro = inro;
+    }
+
+    public int getInum() {
+        return inum;
+    }
+
+    public void setInum(int inum) {
+        this.inum = inum;
+    }
+
+    public int getIart() {
+        return iart;
+    }
+
+    public void setIart(int iart) {
+        this.iart = iart;
+    }
+
+    public String getiDs1() {
+        return iDs1;
+    }
+
+    public void setiDs1(String iDs1) {
+        this.iDs1 = iDs1;
+    }
+
+    public String getIuvt() {
+        return iuvt;
+    }
+
+    public void setIuvt(String iuvt) {
+        this.iuvt = iuvt;
+    }
+
+    public double getIpru() {
+        return ipru;
+    }
+
+    public void setIpru(double ipru) {
+        this.ipru = ipru;
+    }
+
+    public double getIcom() {
+        return icom;
+    }
+
+    public void setIcom(double icom) {
+        this.icom = icom;
+    }
+
+    public int getIcnt() {
+        return icnt;
+    }
+
+    public void setIcnt(int icnt) {
+        this.icnt = icnt;
+    }
+
+    public double getIbrt() {
+        return ibrt;
+    }
+
+    public void setIbrt(double ibrt) {
+        this.ibrt = ibrt;
+    }
+
+    public double getIdsc() {
+        return idsc;
+    }
+
+    public void setIdsc(double idsc) {
+        this.idsc = idsc;
+    }
+
+    public double getItai() {
+        return itai;
+    }
+
+    public void setItai(double itai) {
+        this.itai = itai;
+    }
+
+    public double getIigv() {
+        return iigv;
+    }
+
+    public void setIigv(double iigv) {
+        this.iigv = iigv;
+    }
+
+    public double getItnt() {
+        return itnt;
+    }
+
+    public void setItnt(double itnt) {
+        this.itnt = itnt;
+    }
+
+    public String getIest() {
+        return iest;
+    }
+
+    public void setIest(String iest) {
+        this.iest = iest;
+    }
+    
+    
+}
